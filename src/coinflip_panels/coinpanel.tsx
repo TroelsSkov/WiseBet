938560a3f581ee3894ad7f134e5e86dc9d940e6b
Simon Hedengran Jensen <simon.hedengran@gmail.com>
2026-03-25 16:45:59 +0100

    coinflip optimized to fetch from backend

--- src/coinflip_panels/coinpanel.tsx
+++ src/coinflip_panels/coinpanel.tsx
@@ -2,50 +2,84 @@ import { useEffect, useState } from "react";
 import "./coinpanel.css";
 
 type BetData = {
-  amount: number;
-  choice: "plat" | "krone";
+    amount: number;
+    choice: "W" | "B";
 };
 
 type Props = {
-  betData: BetData | null;
-  shouldFlip: boolean;
-  onFlipped: () => void;
+    betData: BetData | null;
+    shouldFlip: boolean;
+    onFlipped: () => void;
 };
 
+const playRound = async (amount: number, choice: "W" | "B") => {
+    const res = await fetch("http://localhost:3001/api/play-round", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ amount, choice }),
+    });
+
+    return await res.json();
+};
+
+
 export default function Coin({ betData, shouldFlip, onFlipped }: Props) {
-  const [rotation, setRotation] = useState(0);
-  const [flipping, setFlipping] = useState(false);
+    const [rotation, setRotation] = useState(0);
+    const [flipping, setFlipping] = useState(false);
 
-  useEffect(() => {
-    if (!shouldFlip || flipping || !betData) return;
+    useEffect(() => {
+        if (!shouldFlip || flipping || !betData) return;
 
-    setFlipping(true);
 
-    const result = Math.random() > 0.5 ? "plat" : "krone";
-    const spins = 17;
-    const final = result === "plat" ? 0 : 180; //front (W)=0, back (B)=180
+        const run = async () => {
+            setFlipping(true);
 
-    setRotation((prev) => prev + spins * 360 + final);
+            try {
+                const { result, winnings } = await playRound(
+                    betData.amount,
+                    betData.choice
+                );
 
-    // (klar til senere: win/lose)
-    // const didWin = result === betData.choice;
-    // console.log("Result:", result, "You chose:", betData.choice, "Win:", didWin);
+                const spins = 17;
+                const final = result === "W" ? 0 : 180; //front (W)=0, back (B)=180
 
-  }, [shouldFlip, betData, flipping]);
+                // setRotation((prev) => prev + spins * 360 + final + 360);
+                setRotation((prev) => {
+                    const normalized = prev % 360; // 0 eller 180
+                    const spins = 17;
+                    const final = result === "W" ? 0 : 180;
 
-  return (
-    <div className="coinContainer">
-      <div
-        className="coin"
-        style={{ transform: `rotateY(${rotation}deg)` }}
-        onTransitionEnd={() => {
-          setFlipping(false);
-          onFlipped();
-        }}
-      >
-        <div className="side front">W</div>
-        <div className="side back">B</div>
-      </div>
-    </div>
-  );
+                    return prev + spins * 360 + (final - normalized);
+                });
+
+                console.log("Result:", result);
+                console.log("Win:", winnings);
+
+
+            } catch (err) {
+                console.error("Api fejl", err);
+                setFlipping(false);
+            }
+        };
+        run();
+
+    }, [shouldFlip, betData, flipping]);
+
+    return (
+        <div className="coinContainer">
+            <div
+                className="coin"
+                style={{ transform: `rotateY(${rotation}deg)` }}
+                onTransitionEnd={() => {
+                    setFlipping(false);
+                    onFlipped();
+                }}
+            >
+                <div className="side front">W</div>
+                <div className="side back">B</div>
+            </div>
+        </div>
+    );
 }
