7311e9a062ca9818eb4c1938ccb1f98ec8bb4342
Esben Christian Volsgaard <esben.volsgaard@gmail.com>
2026-04-17 00:49:39 +0200

    work on login and auth checking in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Layout from './layouts/MainLayout'
@@ -7,8 +7,24 @@ import Coinflip from './pages/games/Coinflip'
 import Blackjack from './pages/games/Blackjack'
 import Roulette from './pages/games/Roulette'
 import Signup from './pages/Signup'
+import { useEffect } from 'react'
+import { useApi } from './services/useApi'
 
 function App() {
+  const location = useLocation();
+  const excludedPaths = ["/login", "/signup"];
+  const { error } = useApi<void>("/Api/Users/Auth");
+
+  useEffect(() => {
+    if (excludedPaths.includes(location.pathname)) {
+      return;
+    }
+
+    if (error) {
+      console.error("Session check failed:", error);
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <Routes>
