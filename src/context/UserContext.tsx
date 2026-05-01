10fee3f27a64d1b556f14a1a6193aae4c7ebf5bf
Simon Hedengran Jensen <simon.hedengran@gmail.com>
2026-05-01 15:04:34 +0200

    updated login option from mainpage. login is now optional wihtout accessing localhost:8080/login

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,11 +1,47 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect  } from "react";
+import { apiService } from "../services/apiService";
+import type { User } from "../types/user";
 
-type User = {
-  id: string;
+// type User = {
+//   id: string;
+// };
+
+type UserContextType = {
+    user: User | null;
+    setUser: (user: User | null) => void;
 };
 
-export const UserContext = createContext<User | null>(null);
+
+export const UserContext = createContext<UserContextType | null>(null);
+
+export function UserProvider({ children }: { children: React.ReactNode }) {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const { data, error } = await apiService.get<string>("/Api/Users/Auth");
+            if (!error && data) {
+                const username = data.split(": ")[1] ?? "Ukendt";
+                setUser({ username, FullName: "", saldo: 0 });
+            }
+        }
+        fetchUser();
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
 
 export const useUser = () => {
-  return useContext(UserContext);
+    const ctx = useContext(UserContext);
+    if (!ctx) throw new Error("useUser must be used within UserProvider");
+    return ctx;
 };
+
+
+// export const useUser = () => {
+//   return useContext(UserContext);
+// };
