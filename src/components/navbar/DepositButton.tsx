def08e96b938667a448eca514d98d584dc6ab423
Esben Christian Volsgaard <esben.volsgaard@gmail.com>
2026-05-05 13:30:42 +0200

    expand deposit button functionality

--- src/components/navbar/DepositButton.tsx
+++ src/components/navbar/DepositButton.tsx
@@ -5,9 +5,24 @@ import { faPlusCircle } from "@fortawesome/free-solid-svg-icons/faPlusCircle";
 import Modal from "../ui/Modal";
 import Input from "../form/Input";
 import { useState } from "react";
+import { apiService } from "../../services/apiService";
 
 function DepositButton() {
     const [modalVisible, setModalVisible] = useState(false);
+    const [amount, setAmount] = useState("");
+
+    async function handleDeposit(e: React.SubmitEvent) {
+        e.preventDefault();
+
+        const { ok, error } = await apiService.post("/Api/Users/Auth/Login", { amount: amount });
+
+        if (!ok) {
+            // Show error modal
+            return;
+        }
+
+        // show success modal with amount
+    }
 
     return (
         <span>
@@ -17,13 +32,15 @@ function DepositButton() {
                 <p>Indbetal</p>
             </Button>
             <Modal visible={modalVisible} title="Indbetal">
-                <p>Indtast beløb:</p>
-                <Input type="number" placeholder="Beløb at indbetale" />
-                <div className="flex justify-end gap-4">
-                    <Button onClick={() => setModalVisible(false)}>Luk</Button>
-                    <br />
-                    <Button color="green" onClick={() => alert("Indbetalingshandling")}>Indbetal</Button>
-                </div>
+                <form onSubmit={handleDeposit}>
+                    <p>Indtast beløb:</p>
+                    <Input type="number" onChange={e => { setAmount(e.target.value) }} placeholder="Beløb at indbetale" />
+                    <div className="flex justify-end gap-4">
+                        <Button onClick={() => setModalVisible(false)}>Luk</Button>
+                        <br />
+                        <Button color="green">Indbetal</Button>
+                    </div>
+                </form>
             </Modal>
         </span>
     )
