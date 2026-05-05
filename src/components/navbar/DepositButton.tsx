5d61f42cd2f3962a2ba1de41f5c8f1f60b65030d
Esben Christian Volsgaard <esben.volsgaard@gmail.com>
2026-05-05 13:59:43 +0200

    re-write modal and deposit button

--- src/components/navbar/DepositButton.tsx
+++ src/components/navbar/DepositButton.tsx
@@ -6,22 +6,37 @@ import Modal from "../ui/Modal";
 import Input from "../form/Input";
 import { useState } from "react";
 import { apiService } from "../../services/apiService";
+import { toast } from "react-toastify";
 
 function DepositButton() {
+    const notify = (message: string) => toast.success(message);
+    const notifyError = (message: string) => toast.error(message);
     const [modalVisible, setModalVisible] = useState(false);
     const [amount, setAmount] = useState("");
 
     async function handleDeposit(e: React.SubmitEvent) {
         e.preventDefault();
 
-        const { ok, error } = await apiService.post("/Api/Users/Auth/Login", { amount: amount });
-
-        if (!ok) {
-            // Show error modal
+        if (Number(amount) <= 0) {
+            notifyError("Beløbet skal være større end 0.");
             return;
         }
 
-        // show success modal with amount
+        try {
+            const { error } = await apiService.post("/Api/Users/Deposit", { amount: amount });
+
+            if (error) {
+                notifyError(`Indbetaling mislykkedes: ${error}`);
+                return;
+            }
+
+            setModalVisible(false)
+            notify(`Der er indbetalt ${amount} WiC på din konto!`);
+        }
+        catch (err) {
+            console.error(`Indbetaling mislykkedes: ${err}`);
+            notifyError(`Indbetaling mislykkedes: ${err}`);
+        }
     }
 
     return (
@@ -32,12 +47,11 @@ function DepositButton() {
                 <p>Indbetal</p>
             </Button>
             <Modal visible={modalVisible} title="Indbetal">
-                <form onSubmit={handleDeposit}>
-                    <p>Indtast beløb:</p>
-                    <Input type="number" onChange={e => { setAmount(e.target.value) }} placeholder="Beløb at indbetale" />
+                <form onSubmit={handleDeposit} className="space-y-6">
+                    <label htmlFor="amount" className="">Beløb</label>
+                    <Input id="amount" type="number" onChange={e => { setAmount(e.target.value) }} placeholder="Beløb at indbetale" />
                     <div className="flex justify-end gap-4">
                         <Button onClick={() => setModalVisible(false)}>Luk</Button>
-                        <br />
                         <Button color="green">Indbetal</Button>
                     </div>
                 </form>
